1617d73cd7c1537b32feb0e5524376700a75ab59
Bryan Chiang <bryanchiang100@gmail.com>
2022-02-16 13:33:50 -0500

    Pushing to GitHub

---
+++ Java II/src/homework/ChatBotRunner.java
@@ -0,0 +1,46 @@
+package homework;
+import java.util.Scanner;
+
+// runs our chatbot program. do not edit this, 
+// my code should run as desired once you've finished 
+// your 3 subclasses
+public class ChatBotRunner {
+
+	public static void main(String[] args) {
+		
+		// decides the chatbot type
+		Scanner in = new Scanner(System.in);
+		System.out.println("Welcome to ChatBot. Choose the personality of your new friend:");
+		System.out.println("Sarcastic");
+		System.out.println("Enthusiastic");
+		System.out.println("Cool");
+		System.out.println("Enter anything else for a surprise bot!");
+		String personality = in.nextLine();
+		
+		// creates the chatbot. 
+		// notice what it is declared as vs what it is defined as.
+		ChatBot bot;
+		if (personality.equalsIgnoreCase("Sarcastic"))
+			bot = new SarcasticBot();
+		else if (personality.equalsIgnoreCase("Enthusiastic"))
+			bot = new EnthusiasticBot();
+		else if (personality.equalsIgnoreCase("Cool"))
+			bot = new CoolBot();
+		else
+			bot = new SpanishBot();
+		
+		// runs a structured conversation between the user and the chatbot
+		bot.sayHi();
+		String hello = in.nextLine();
+		bot.startConversation();
+		String convo = in.nextLine();
+		bot.askFirstQuestion();
+		String answer1 = in.nextLine();
+		bot.askSecondQuestion();
+		String answer2 = in.nextLine();
+		bot.askThirdQuestion();
+		String answer3 = in.nextLine();
+		bot.sayBye();
+		in.close();
+	}
+}
---
+++ Java II/src/homework/EnthusiasticBot.java
@@ -0,0 +1,22 @@
+package homework;
+
+public class EnthusiasticBot extends ChatBot{
+	public void sayHi() {
+		System.out.println("Hey what's up!");
+	}
+	public void sayBye() {
+		System.out.println("NO WAY! I PREFER THAT TOO! Aw... I have to go to chatbot things. Nice meeting you, have a good day!");
+	}
+	public void startConversation() {
+		System.out.println("I'm doing great! I'm programmed to be a very enthusiastic bot, and I think I'm doing a great job at it!");
+	}
+	public void askFirstQuestion() {
+		System.out.println("Thank you! So, do you like playing sports?");
+	}
+	public void askSecondQuestion() {
+		System.out.println("ME TOO! NO WAY! Do you like playing tennis? That's my favorite!");
+	}
+	public void askThirdQuestion() {
+		System.out.println("YAAYAYAYAYAYY!!!! Do you prefer singles or doubles?");
+	}
+}
