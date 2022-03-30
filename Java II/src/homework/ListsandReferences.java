496f7f1391e0fc0e63659257218909371b6ab8a0
Bryan Chiang <bryanchiang100@gmail.com>
2022-03-30 13:16:08 -0400

    pushing to GitHub

--- Java II/src/homework/ListsandReferences.java
+++ Java II/src/homework/ListsandReferences.java
@@ -11,22 +11,51 @@ public class ListsandReferences {
 		System.out.println(answer);
 	}
 	public void average(ArrayList<Double> A) {
-		double average = 0;
 		ArrayList<Double> answer = new ArrayList<Double> ();
-		for (int i = 0; i < A.size(); i++) {
-			average += A.get(i);
+		int index = 0;
+		while (answer.size() != A.size()) {
+			double average = 0;
+			for (int i = 0; i < A.size(); i++) {
+				average += A.get(i);
+			}
+			average /= A.size();
+			answer.add(average);
+			A.remove(index);
+			A.add(index, average);
+			index++;
 		}
-		average /= A.size();
-		answer.add(average);
+		System.out.println(answer);
+	}
+	public void add(ArrayList<Integer> A) {
+		ArrayList<Integer> answer = new ArrayList<Integer> ();
+		for (int i = 0; i < A.size(); i++) {
+			for (int j = 0; j < i + 1 || j == 0; j++) {
+				answer.add(A.get(j));
+			}
+		}
+		System.out.println(answer);
 	}
 	public static void main(String [] args) {
 		ListsandReferences runner = new ListsandReferences();
-		ArrayList<Character> test = new ArrayList<Character> ();
-		test.add('h');
-		test.add('e');
-		test.add('l');
-		test.add('l');
-		test.add('o');
-		runner.concatenate(test); 
+		ArrayList<Character> test1 = new ArrayList<Character> ();
+		test1.add('h');
+		test1.add('e');
+		test1.add('l');
+		test1.add('l');
+		test1.add('o');
+		runner.concatenate(test1); 
+		ArrayList<Double> test2 = new ArrayList<Double> ();
+		test2.add(3.0);
+		test2.add(6.0);
+		test2.add(1.0);
+		test2.add(8.0);
+		test2.add(19.0);
+		runner.average(test2);
+		ArrayList<Integer> test3 = new ArrayList<Integer> ();
+		test3.add(3);
+		test3.add(1);
+		test3.add(4);
+		test3.add(2);
+		runner.add(test3);
 	}
 }
