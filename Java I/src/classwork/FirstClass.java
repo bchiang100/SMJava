b4b8de63abb53d83ec938c56d87ab47cd0223aae
bryan <bryan@Bryan_Zenbook14>
2023-07-17 20:14:06 +0800

    pushing to GitHub

--- Java I/src/classwork/FirstClass.java
+++ Java I/src/classwork/FirstClass.java
@@ -3,9 +3,5 @@ package classwork;
 public class FirstClass {
 	public static void main (String [] args) {
 		System.out.println("hello world");
-<<<<<<< HEAD
-=======
-		//test
->>>>>>> branch 'master' of https://github.com/bchiang100/SMJava
 	}
 }
